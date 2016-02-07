4c25fec8de322e21939044d34b03ca2e3321bd70
Oliver Friedmann <oliver.friedmann@gmail.com>
2016-02-07 13:37:29 -0500

    Add Connect to Browser Qunit

--- grunt/browserqunit.js
+++ grunt/browserqunit.js
@@ -1,9 +1,25 @@
-module.exports = function (taskname, test) {
-	this.grunt.loadNpmTasks('grunt-shell');
+module.exports = function (taskname, test, httpserver) {
 	taskname = taskname || "browserqunit";
-	return this.registerTask(taskname, [
-		this.addConfigTask("shell", taskname, {
-			command: "open " + test
-		})
-	]);
+	if (httpserver) {
+		this.grunt.loadNpmTasks('grunt-contrib-connect');
+		return this.registerTask(taskname, [
+			this.addConfigTask("connect", taskname, {
+				options: {
+					  port: 8711,
+					  base: '.',
+					  keepalive: true,
+					  open: {
+						   target: 'http://localhost:8711/' + test
+					  }
+				}
+			})
+		]);
+	} else {
+		this.grunt.loadNpmTasks('grunt-shell');
+		return this.registerTask(taskname, [
+			this.addConfigTask("shell", taskname, {
+				command: "open " + test
+			})
+		]);
+	}
 };
