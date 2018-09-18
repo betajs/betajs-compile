2e1f9d242f1106bf36884af9440b7ff18b17f544
Oliver Friedmann <oliver.friedmann@gmail.com>
2018-09-18 23:50:44 +0200

    Improve Scoped Version Task

--- grunt/scopedclosurerevision.js
+++ grunt/scopedclosurerevision.js
@@ -1,18 +1,21 @@
-module.exports = function (taskname, source, target, bindings, version_assumptions, exports, process) {
-	this.grunt.loadNpmTasks('grunt-preprocess');
+module.exports = function (taskname, source, target, bindings, version_assumptions, exports, process, options) {
+	options = options || {};
+	options.module_name = options.module_name || "module";
+    this.grunt.loadNpmTasks('grunt-preprocess');
 	this.grunt.loadNpmTasks('betajs-scoped');
 	taskname = taskname || "scopedclosurerevision";
+	var defines = {};
+    defines[options.module_name + ":"] = {
+        guid : this.pkg.meta.guid,
+		version: this.pkg.version,
+		datetime: (new Date()).getTime()
+    };
 	return this.registerTask(taskname, [
 		this.addConfigTask("scoped-closure", taskname, {
 			bindings: bindings,
 			version_assumptions: version_assumptions,
-			defines : {
-				"module:" : {
-					guid : this.pkg.meta.guid,
-					version: this.pkg.version
-				}
-			},
-			exports: exports ? "module:" : false,
+			defines : defines,
+			exports: exports ? (options.module_name + ":") : false,
 			src : source,
 			dest : target,
 			banner : this.banner,
