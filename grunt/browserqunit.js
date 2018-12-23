f8d89b69da3eae91ca06998405c8e41f94e32f08
Oliver Friedmann <oliver.friedmann@gmail.com>
2018-12-22 20:15:26 -0500

    Ngrok in Browser Qunit

--- grunt/browserqunit.js
+++ grunt/browserqunit.js
@@ -1,8 +1,18 @@
-module.exports = function (taskname, test, httpserver) {
+module.exports = function (taskname, test, httpserver, ngrok) {
 	taskname = taskname || "browserqunit";
 	if (httpserver) {
 		this.grunt.loadNpmTasks('grunt-contrib-connect');
+		if (ngrok) {
+			this.grunt.loadNpmTasks('grunt-shell');
+			this.registerTask(taskname + "-ngrok", function () {
+				require("child_process").spawn("ngrok", ["http", "8711"]);
+			});
+		}
 		return this.registerTask(taskname, [
+			ngrok ? taskname + "-ngrok" : null,
+			ngrok ? this.addConfigTask("shell", taskname, {
+				command: "open http://127.0.0.1:4040"
+			}) : null,
 			this.addConfigTask("connect", taskname, {
 				options: {
 					  port: 8711,
